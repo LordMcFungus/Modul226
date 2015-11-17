1e84a23e672b7069b3f8e5c9b225321cb3082bd4
Alain Keller <alain.keller@edu.tbz.ch>
2015-11-17 10:29:34 +0100

    bla

---
+++ BasciTest/BasciTest/BasischTescht.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasciTest
+{
+	class BasischTescht
+	{
+		private ICarBase Car;
+		private Drivers driver = new Drivers();
+		private Cars cars = new Cars();
+		public BasischTescht()
+		{
+			DelegateCar();
+		}
+
+		private void DelegateCar()
+		{
+			Car = cars.CarSet.First();
+			Car.driver = driver.drivers.Dequeue();
+			Car.Drive();
+		}
+	}
+}
